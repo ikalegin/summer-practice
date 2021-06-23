8314d944c4b42a7d1e232e60b33bd2a00bba198e
ikalegin <86369727+ikalegin@users.noreply.github.com>
2021-06-23 18:58:32 +0300

    Update unittest.c

--- unittestlib/unittest.c
+++ unittestlib/unittest.c
@@ -24,7 +24,7 @@ int main() {
 	double expDy[3] = { 4.38, 7.13,10 };
 	int expOut[10] = { 1, 0, 0, 0, 0, 1, 1, 0, 1, 2};
 
-	//ТЕСТЫ НА ПРОВЕРКУ, ТРЕУГОЛЬНИК ЛИ
+	//Г’Г…Г‘Г’Г› ГЌГЂ ГЏГђГЋГ‚Г…ГђГЉГ“, Г’ГђГ…Г“ГѓГЋГ‹ГњГЌГ€ГЉ Г‹Г€
 
 	for (int i = 0; i < 7; i++) {
 		expected_output = expOut[i];
@@ -33,7 +33,7 @@ int main() {
 		ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
 	}
 
-	// ТЕСТЫ НА ПРОВЕРКУ ВХОДИМОСТИ ТОЧКИ В ТРЕУГОЛЬНИК
+	// Г’Г…Г‘Г’Г› ГЌГЂ ГЏГђГЋГ‚Г…ГђГЉГ“ Г‚Г•ГЋГ„Г€ГЊГЋГ‘Г’Г€ Г’ГЋГ—ГЉГ€ Г‚ Г’ГђГ…Г“ГѓГЋГ‹ГњГЌГ€ГЉ
 
 	for (int i = 7; i < 10; i++) {
 		expected_output = expOut[i];
@@ -41,54 +41,5 @@ int main() {
 		obtainted_output = isintriangle(Ax, Bx, Cx, Ay, By, Cy, Dx, Dy);
 		ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
 	}
-	/*// ТЕСТЫ НА ПРОВЕРКУ ФУНКЦИИ istriangle
-	double Ax = 1, Bx = 2, Cx = 5, Ay = 1, By = 5, Cy = 3; // проверка на работоспособность
-	int expected_output = 1, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = 1, Bx = 2, Cx = 3, Ay = 1, By = 2, Cy = 3; // тест на проверку работы функции на распознавание прямой в первой четверти
-	expected_output = 0, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = -9.2, Bx = -5, Cx = -3.36, Ay = 14.2, By = 10, Cy = 8.36; // тест на проверку работы функции на распознавание прямой во второй четверти
-	expected_output = 0, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = -6.67, Bx = -12.02, Cx = -1.13, Ay = -3.335, By = -6.01, Cy = -0.565; // тест на проверку работы функции на распознавание прямой в третьей четверти
-	expected_output = 0, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = 2, Bx = 7, Cx = 12, Ay = 0, By = -10, Cy = -20; // тест на проверку работы функции на распознавание прямой в четвертой четверти
-	expected_output = 0, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = -20, Bx = 6, Cx = -9, Ay = -5, By = 21, Cy = 17; // проверка на работу с отрицательными числами
-	expected_output = 1, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = -7.23, Bx = 12.741, Cx = -6.31, Ay = 2.23, By = 9.216, Cy = 8.12; // проверка на работу с отрицательными числами
-	expected_output = 1, obtainted_output;
-	obtainted_output = istriangle(Ax, Bx, Cx, Ay, By, Cy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-
-	// ТЕСТЫ НА ПРОВЕРКУ ФУНКЦИИ isintriangle
-	double Dx, Dy;
-	Ax = 4.6, Bx = 6.3, Cx = 12.14, Ay = -1.02, By = 4.38, Cy = 2.58, Dx = 7.78, Dy = 4.38; // проверка на невходимость точки в треугольник
-	expected_output = 0, obtainted_output;
-	obtainted_output = isintriangle(Ax, Bx, Cx, Ay, By, Cy, Dx, Dy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-
-	Ax = 4.6, Bx = 6.3, Cx = 12.14, Ay = -1.02, By = 4.38, Cy = 2.58, Dx = 7.78, Dy = 4.38; // проверка на невходимость точки в треугольник
-	expected_output = 0, obtainted_output;
-	obtainted_output = isintriangle(Ax, Bx, Cx, Ay, By, Cy, Dx, Dy);
-	ASSERT(expected_output != obtainted_output, "Test failed. Doesn't work for your coordinates.");
-	*/
 	return 0;
 }
